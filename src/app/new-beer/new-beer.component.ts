c98a867fc474e33437f9aa00d38fac8c262ef205
cb <csbutton01@hotmail.com>
2018-05-03 08:23:22 -0700

    make new components for keg and new-beer

---
+++ src/app/new-beer/new-beer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-beer',
+  templateUrl: './new-beer.component.html',
+  styleUrls: ['./new-beer.component.css']
+})
+export class NewBeerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
